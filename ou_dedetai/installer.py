41c7588005d80d5e8663201db51207aa81e74e4f
Nathan Shaaban <86252985+ctrlaltf24@users.noreply.github.com>
2025-10-13 18:08:41 -0700

    fix: ensure wine appimage is downloaded
    Not sure why this didn't pop up earlier in testing, but for some reason it appeared on my machine so added an additional check to make sure the appimage downloads

--- ou_dedetai/installer.py
+++ ou_dedetai/installer.py
@@ -198,7 +198,11 @@ def ensure_appimage_download(app: App):
     check_system_compatibility(app=app)
     app.installer_step += 1
 
-    if app.conf.faithlife_product_version != '9' and not str(app.conf.wine_binary).lower().endswith('appimage'):
+    if (
+        app.conf.faithlife_product_version != '9'
+        and not str(app.conf.wine_binary).lower().endswith('appimage')
+        and app.conf.wine_binary not in [constants.WINE_RECOMMENDED_SIGIL, constants.WINE_BETA_SIGIL]
+    ):
         return
     app.status("Ensuring wine AppImage is downloaded…")
 
